3e9739ad49ce80a7dba3caf77af982862f3e69ce
pengqin <jerry@jollycorp.com>
2018-12-06 15:19:14 +0800

    类目页面

--- my-app/webpack.config.js
+++ my-app/webpack.config.js
@@ -2,17 +2,28 @@
  * Created by jerry on 2018/11/27.
  */
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');// 自动生成index.html文件，所有的 bundle 会自动添加到 html 中。
+
 function resolve (dir) {
 	return path.join(__dirname, '..', dir);
 }
 module.exports = {
 	mode   : 'production',
-	entry  : path.resolve(__dirname, 'src/index.js'),
+	entry : {
+		app   : path.resolve(__dirname, 'src/index.js'),
+		home : path.resolve(__dirname, 'src/home.js')
+	},
 	output : {
 		// webpack 如何输出结果的相关选项
 		path     : path.resolve(__dirname, 'dist'), // string
-		filename : 'index.js'  // 打包后文件
+		// 根据entry中入口的js，输出对应的文件名称
+		filename: '[name].bundle.js'
 	},
+	plugins : [
+		new HtmlWebpackPlugin({
+			title : 'Output Management'
+		})
+	],
 	module : {
 		rules : [
 			// 配置js/jsx语法解析
@@ -33,5 +44,9 @@ module.exports = {
 				}
 			},
 		]
+	},
+	// webpack热部署的另一种启动方式, 配合package中的start-dev命令一起使用, 访问目录是dis文件夹
+	devServer : {
+		contentBase : './dist'
 	}
 }
