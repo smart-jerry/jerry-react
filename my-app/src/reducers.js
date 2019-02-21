0686bdccd00a50f5a4ceb8df72312dec688dcade
pengqin <jerry@jollycorp.com>
2019-02-21 18:59:04 +0800

    弄清redux目录划分

--- my-app/src/reducers.js
+++ my-app/src/reducers.js
@@ -2,7 +2,7 @@
  * Created by jerry on 2019/1/16.
  */
 import { combineReducers } from 'redux'
-import demoRedux from '../demo/reducers/index'
+import demoRedux from './demo/reducers/index'
 //import cartRedux from '../cart/reducers/index'
 
 const rootReducer = combineReducers(demoRedux)
