bd5e5a0944bf477fb925b09be194dc1efbcdbd29
pengqin <jerry@jollycorp.com>
2019-02-26 15:38:05 +0800

    购物车计算

--- my-app/src/cart/reducers/cartList.js
+++ my-app/src/cart/reducers/cartList.js
@@ -7,6 +7,26 @@
 let stateList =[
 	{id:1,num:1,price:'1.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
 	{id:2,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:3,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:4,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:5,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:6,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:7,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:8,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:9,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:10,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:11,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:12,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:13,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:14,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:15,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:16,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:17,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:18,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:19,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:20,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:21,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
+	{id:22,num:2,price:'2.3',currency:'$',info:'Mini Face Cleaner Ultrasonic Pulse Silicone Electric Cleansing Apparatus',sku:' SIZE:Free / Blue', img:'https://imcut.jollychic.com//uploads/jollyimg/gallery/201807/19/IL20180719152802887.jpg'},
 ]
 	
 const cartList = (state = stateList, action) => {
