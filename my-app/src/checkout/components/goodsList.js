4bb92a46c88292ef8f4976a4379280b4bf4dc252
pengqin <jerry@jollycorp.com>
2019-03-05 15:38:32 +0800

    checkout页面

--- my-app/src/checkout/components/goodsList.js
+++ my-app/src/checkout/components/goodsList.js
@@ -2,14 +2,42 @@
  * Created by jerry on 2019/3/5.
  */
 import React from 'react';
-const goodsList = ({ checkoutList, minusNum, addNum}) => (
-<ul className="cart-box">
-	{checkoutList.map(goods => (
-		<li>
-			<div className="img-box"><img className="img-auto-width" src={goods.img} /></div>
-		</li>
-	))}
-</ul>
+const goodsList = ({ goodsList, subTotal, minusNum, addNum}) => (
+<div className="checkout-box">
+	<div className="checkout-info">
+		<ul className="goods-list">
+			{goodsList.map(goods => (
+				<li>
+					<div className="img-box"><img className="img-auto-width" src={goods.img} /></div>
+				</li>
+			))}
+		</ul>
+		<div className="info">Shipping Charge:{subTotal.charge} {subTotal.currency}</div>
+		<div className="info">Delivery Time:{subTotal.deliveryTime}</div>
+		<div className="info">Product Total:{subTotal.totalnum}</div>
+		<div className="info">Sub Total:{subTotal.total} {subTotal.currency}</div>
+		<div style={{'height':'200px'}}>
+			
+			test
+		</div>
+		<div style={{'height':'200px'}}>
+			
+			test
+		</div>
+		<div style={{'height':'200px'}}>
+			
+			test
+		</div>
+		<div style={{'height':'200px'}}>
+			
+			test
+		</div>
+	</div>
+	<div className="order-box">
+		<div className="grand-total">Grand Total: {subTotal.total} {subTotal.currency}</div>
+		<div className="order-btn">Proceed to Payment</div>
+	</div>
+</div>
 )
 
 export default goodsList
--- my-app/src/checkout/container/Checkout.List.js
+++ my-app/src/checkout/container/Checkout.List.js
@@ -8,8 +8,25 @@ import goodsList from '../components/goodsList'
 
 // 负责输入逻辑，即将state映射到 UI 组件的参数（props）
 const mapStateToProps = state => {
+	let total=0; // 总金额
+	let currency=''; // 币种符号
+	let totalnum=0;// 总数量
+	state.checkoutList.map(cart=>{
+		if(!currency){
+			currency = cart.currency;
+		}
+		total+=cart.price*cart.num
+		totalnum += cart.num;
+	})
 	return {
-		checkoutList: state.checkoutList // ui组件中的参数===json的key值
+		goodsList: state.checkoutList, // ui组件中的参数===json的key值
+		subTotal:{
+			total:total,
+			totalnum:totalnum,
+			currency:currency,
+			charge:'20',
+			deliveryTime:255555555
+		}
 	}
 }
 // 负责输出逻辑，即将用户对 UI 组件的操作映射成 Action
