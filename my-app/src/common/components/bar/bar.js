97eacc41b7941185c5329c4614905d83c7440903
pengqin <jerry@jollycorp.com>
2019-03-14 11:37:41 +0800

    页签选中

--- my-app/src/common/components/bar/bar.js
+++ my-app/src/common/components/bar/bar.js
@@ -2,29 +2,32 @@
  * Created by jerry on 2018/12/5.
  */
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink  } from 'react-router-dom';
 import "./bar.less";
-// 倒计时
+// icon图标
 class Bar extends Component {
 	// data
 	constructor(props) {
 		super(props);
-		console.log(props,'===================props');
-		this.match = props.match;
-		this.selectIcon = props.currentPathName;
 		// 底部导航
-		this.barlist = props.barlist || []
+		this.barlist = props.barlist || [];
+		this.state = {selectIcon: '/index'};
 	}
-	
+	changeIcon(s) {
+		this.setState({
+			selectIcon : s
+		});
+	}
 	render() {
 		return (<ul className="footer-bar">
 			{
-				this.barlist.map((item) =>
-					<li>
-						<Link to={item.href}>
-							<div className="img-box"><img className="img-auto-width" src={this.selectIcon === item.name?item.imgurl_on:item.imgurl} /></div>
+				this.barlist.map((item,index) =>
+					<li onClick = {this.changeIcon.bind(this,item.href)}>
+						{/*样式可以选用activeClassName="selected"或者isActive={this.myEvent}*/}
+						<NavLink to={item.href} exact className={this.state.selectIcon === item.href ? "selected" : ''}>
+							<div className="img-box"><img className="img-auto-width" src={this.state.selectIcon === item.href?item.imgurl_on:item.imgurl} /></div>
 							<div className="bar-name">{item.name}</div>
-						</Link>
+						</NavLink>
 					</li>
 				)
 			}
